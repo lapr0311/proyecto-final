49d46fa9b438578af7ae5bf43361ffcfae0adaa1
Pilar Abarca <mariapilar.abarca@gmail.com>
2023-04-25 17:42:31 -0400

    eliminacion de contenido de context

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Perfil from "../pages/Perfil";
 import Register from "../pages/Register";
+import { PrivateRoute } from "./PrivateRoute";
 // import PizzaDetails from "../pages/PizzaDetails";
 
 const Routers = () => {
@@ -17,11 +18,18 @@ const Routers = () => {
 		<Routes>  
 			<Route path="/" element={<Home/>}/>
 			<Route path="/login" element={<Login/>}/>
-			<Route path="/perfil" element={<Perfil/>}/>
+	
 			<Route path="/Register" element={<Register/>}/>
 			{/* <Route path="/pizza/:id" element={<PizzaDetails/>}/> */}
 			{/* <Route path="*" element={<NotFound />} /> */}
-
+			<Route
+              path="/Perfil"
+              element={
+                <PrivateRoute>
+                  <Perfil />
+                </PrivateRoute>
+              }
+            />
 		</Routes>
 			
 		
